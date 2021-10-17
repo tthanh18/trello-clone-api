690a5cd8c915243994c55cf163e526680e9fc08c
Thanh Nguyen <ng.tthanh18@gmail.com>
2021-10-17 19:51:02 +0700

    Config babel

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { mapOrder } from '*/utilities/sorts'
 
 const app = express()
 
@@ -6,9 +7,9 @@ const hostName = 'localhost'
 const port = 8080
 
 app.get('/', (req, res) => {
-    res.end('<h1>Hello world</h1>')
+    res.end('<h1>Hello world!</h1>')
 })
 
 app.listen(port, hostName, () => {
-    console.log(`Trello clone app, running at port ${hostName}:${port}/`)
+    console.log(`Trello clone app, running at port http://${hostName}:${port}/`)
 })
--- src/utilities/sorts.js
+++ src/utilities/sorts.js
@@ -2,8 +2,7 @@
     mapOder() : Order an array of object based on another array order
 
 */
-const mapOrder = (arr, order, key) => {
+export const mapOrder = (arr, order, key) => {
     arr.sort((a, b) => order.indexOf(a[key]) - order.indexOf(b[key]))
     return arr
 }
-export { mapOrder }
